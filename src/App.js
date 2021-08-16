29be72b6a69dbc4bf14d0900d4807c01ef07614b
Karthiseeni <karthiseeni96@gmail.com>
2021-08-16 13:51:06 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,3 +9,25 @@ export default function App() {
     </div>
   );
 }
+import Navigation from './components/Navigation/Navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Footer from './components/Footer/Footer';
+import Home from './pages/Homepage/Home';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          {/* <Route path='/products' component={Product}></Route>
+          <Route path='/services' component={Services}></Route>
+          <Route path='/signup' component={SignUp}></Route> */}
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
